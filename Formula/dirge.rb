5b3b2c590b2c89e9abca47ea4cf6c3895d190be7
Yogthos <yogthos@gmail.com>
2026-06-10 17:09:25 -0400

    dirge 0.5.0: real sha256s, wrap linux url in on_intel

--- Formula/dirge.rb
+++ Formula/dirge.rb
@@ -7,17 +7,19 @@ class Dirge < Formula
   on_macos do
     on_arm do
       url "https://github.com/dirge-code/dirge/releases/download/v0.5.0/dirge-aarch64-apple-darwin.tar.gz"
-      sha256 "PASTE_FROM_.sha256"
+      sha256 "c8d88b8b605953bf8f72512cbfd96f51f8507a8d005344163b4681e6a416c4b7"
     end
     on_intel do
       url "https://github.com/dirge-code/dirge/releases/download/v0.5.0/dirge-x86_64-apple-darwin.tar.gz"
-      sha256 "PASTE_FROM_.sha256"
+      sha256 "cab338fba0cb54252f56f9f3f11ee10cc74a5688f5755f21f62dee7f45da6839"
     end
   end
 
   on_linux do
-    url "https://github.com/dirge-code/dirge/releases/download/v0.5.0/dirge-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "PASTE_FROM_.sha256"
+    on_intel do
+      url "https://github.com/dirge-code/dirge/releases/download/v0.5.0/dirge-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "088d5896bcb83b35b1868d5a9d1890a5d0fbcf87636682208b8de9e996562e02"
+    end
   end
 
   def install
@@ -25,6 +27,6 @@ class Dirge < Formula
   end
 
   test do
-    assert_match "dirge", shell_output("#{bin}/dirge --help 2>&1", 0)
+    assert_match "dirge", shell_output("#{bin}/dirge --help 2>&1")
   end
 end
